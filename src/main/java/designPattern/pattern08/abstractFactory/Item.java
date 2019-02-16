89b7aaa7df288792d93ad666d681931f30478239
InSeong-So <goflvhxj2547@gmail.com>
2019-02-16 21:52:54 +0900

    Abstract Factory 패턴 구현

---
+++ src/main/java/designPattern/pattern08/abstractFactory/Item.java
@@ -0,0 +1,12 @@
+package designPattern.pattern08.abstractFactory;
+
+public abstract class Item {
+
+	protected String caption;
+
+	public Item(String caption) {
+		this.caption = caption;
+	}
+
+	public abstract String makeHTML();
+}
---
+++ src/main/java/designPattern/pattern08/abstractFactory/Link.java
@@ -0,0 +1,11 @@
+package designPattern.pattern08.abstractFactory;
+
+public abstract class Link extends Item {
+
+	protected String url;
+
+	public Link(String caption, String url) {
+		super(caption);
+		this.url = url;
+	}
+}
