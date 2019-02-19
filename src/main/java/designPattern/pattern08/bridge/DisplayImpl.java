281aa93efe8a9ad18af31e8e933724a58812b0a9
InSeong-So <goflvhxj2547@gmail.com>
2019-02-19 22:39:01 +0900

    Bridge 패턴 추가

---
+++ src/main/java/designPattern/pattern08/bridge/DisplayImpl.java
@@ -0,0 +1,10 @@
+package designPattern.pattern08.bridge;
+
+public abstract class DisplayImpl {
+	public abstract void rawOpen();
+
+	public abstract void rawPrint();
+
+	public abstract void rawClose();
+
+}
