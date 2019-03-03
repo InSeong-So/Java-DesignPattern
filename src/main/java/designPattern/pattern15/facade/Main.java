d0e5f817674aa31be43beec7e7b9a5220d0f9818
InSeong-So <goflvhxj2547@gmail.com>
2019-03-03 23:44:42 +0900

    Facade 패턴 추가

---
+++ src/main/java/designPattern/pattern15/facade/Main.java
@@ -0,0 +1,8 @@
+package designPattern.pattern15.facade;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PageMaker.makeWelcomePage("goflvhxj2547@naver.com", "welcome.html");
+	}
+}
