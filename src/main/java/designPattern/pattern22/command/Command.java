17de2d78d825b797f3eb852993250e41eeb065c4
InSeong-So <goflvhxj2547@gmail.com>
2019-03-24 15:07:15 +0900

    Command 패턴 추가

---
+++ src/main/java/designPattern/pattern22/command/Command.java
@@ -0,0 +1,6 @@
+package designPattern.pattern22.command;
+
+public interface Command {
+
+	public abstract void execute();
+}
---
+++ src/main/java/designPattern/pattern22/command/Drawable.java
@@ -0,0 +1,6 @@
+package designPattern.pattern22.command;
+
+public interface Drawable {
+
+	public abstract void draw(int x, int y);
+}
