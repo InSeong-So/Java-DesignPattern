03565684832755da1c34917e156bb2b17b060871
InSeong-So <goflvhxj2547@gmail.com>
2019-03-31 18:04:55 +0900

    Interpreter 패턴 추가

---
+++ src/main/java/designPattern/pattern23/interpreter/Node.java
@@ -0,0 +1,6 @@
+package designPattern.pattern23.interpreter;
+
+public abstract class Node {
+
+	public abstract void parse(Context context) throws ParseException;
+}
---
+++ src/main/java/designPattern/pattern23/interpreter/ParseException.java
@@ -0,0 +1,8 @@
+package designPattern.pattern23.interpreter;
+
+public class ParseException extends Exception {
+
+	public ParseException(String msg) {
+		super(msg);
+	}
+}
